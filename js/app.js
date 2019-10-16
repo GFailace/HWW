8e1ab31cb34b2ed6df63b0151f94755af08d73d7
Guilherme Failace <guilherme.failace@gmail.com>
2019-10-16 11:10:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,12 +62,3 @@ function menu() {
     x.style.display = "block";
   }
 }
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
